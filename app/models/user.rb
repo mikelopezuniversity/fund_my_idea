4e143d88e6aef3e043dd17feb9f05646da6ee82c
John Nelson <john@mikelopezuniversity.ml>
2020-09-09 02:28:53 -0700

    Add username to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :projects, dependent: :destroy
 
-  def can_receive_payments?
-    uid? &&  provider? && access_code? && publishable_key?
-  end
+  has_many :projects, dependent: :destroy
 
 end

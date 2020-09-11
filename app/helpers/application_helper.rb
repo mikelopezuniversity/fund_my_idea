2f84edd5d82209638d0cfaf7e61926f050cb000e
John Nelson <john@mikelopezuniversity.ml>
2020-09-10 21:47:34 -0700

    Add action text and Description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,5 +2,7 @@ module ApplicationHelper
   def author_of(resource)
     user_signed_in? && resource.user_id == current_user.id
   end
-
+  def admin?
+    user_signed_in? && current_user.admin?
+  end
 end

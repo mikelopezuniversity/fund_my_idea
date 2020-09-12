182c1e791f0778d64e94675a988a06f5a7679132
John Nelson <john@mikelopezuniversity.ml>
2020-09-12 02:36:06 -0700

    Fix typos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,10 +8,9 @@ module ApplicationHelper
   def stripe_url
     "https://connect.stripe.com/oauth/authorize?response_type=code&client_id=#{Rails.application.credentials.dig(:stripe, :connect_client_id)}&scope=read_write"
 
-    def stripe_connect_button # add this method
-     link_to stripe_url, class: "btn-stripe-connect" do
-       content_tag :span, "Connect with Stripe"
-     end
+  def stripe_connect_button
+    link_to stripe_url, class: "btn-stripe-connect" do
+      content_tag :span, "Connect with Stripe"
+    end
   end
-end
 end

a923f05f8decf04b0eebee6280cc4dbcb6c40e6b
John Nelson <john@mikelopezuniversity.ml>
2020-09-12 07:28:02 -0700

    Fix logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
     user_signed_in? && current_user.admin?
   end
   def stripe_url
-    https://connect.stripe.com/oauth/authorize?response_type=code&client_id='<%= Rails.application.credentials.stripe[:connect_client_id]'&scope=read_write
+    https://connect.stripe.com/oauth/authorize?response_type=code&client_id=<%= Rails.application.credentials.stripe[:connect_client_id]&scope=read_write
   end
   def stripe_connect_button
     link_to stripe_url, class: "btn-stripe-connect" do

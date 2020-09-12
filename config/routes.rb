6ec8be06b0116fd23a01c181c908200a062d3a5c
John Nelson <john@mikelopezuniversity.ml>
2020-09-12 01:23:40 -0700

    Add Oauth Logic

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     resources :comments, module: :projects
   end
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
+
   root to: "projects#index"
 end

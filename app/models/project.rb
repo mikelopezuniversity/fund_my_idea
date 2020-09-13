1490b5f8b12124a50e47a5f9ed7b0b17908e05c5
John Nelson <john@mikelopezuniversity.ml>
2020-09-13 01:52:00 -0700

    Add Perks to Projects Model

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,6 +4,10 @@ class Project < ApplicationRecord
   has_one_attached :thumbnail
   has_many :comments, as: :commentable
 
+  has_many :perks, dependent: :destroy
+  accepts_nested_attributes_for :perks, allow_destroy: true, reject_if: proc { |attr| attr['title'].blank? }
+
+
   scope :active, ->{ where(status: "active") }
   scope :inactive, ->{ where(status: "inactive") }
 
